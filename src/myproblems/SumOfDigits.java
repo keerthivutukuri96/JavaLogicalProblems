690c14145c2175e02d3aac8b8ff6f4cbcd9e4870
keerthi vutukuri <keerthivutukuri@gmail.com>
2023-04-07 06:12:57 +0100

    added even or logic code

--- src/myproblems/SumOfDigits.java
+++ src/myproblems/SumOfDigits.java
@@ -6,22 +6,29 @@ public class SumOfDigits {
 
 	public static void main(String[] args) {
 		
-
+			int choice;
 			System.out.println("Enter a Number:");
 			Scanner scanner = new Scanner(System.in);
 			int digit, num = scanner.nextInt();
+			System.out.println("For Even Enter 1 , For Odd Enter 2");
+			choice = scanner.nextInt();
 			
 			
-			int sum = 0;
 
+			int even = 0;
+			int odd = 0;
 			while (num != 0) {
-				digit = num % 10;
-				sum = sum + digit;
-				// System.out.print(digit+" ");
-
-				num = num / 10;
+				digit = num % 10;		
+				if(digit%2 == 0) {
+					even = even + digit;
+				}
+				else {
+						odd = odd + digit;
+					}
+					num = num / 10;
 			}
-			System.out.println(sum);
+			if(choice == 1) System.out.println("Sum of Even Numbers: " + even);
+			else System.out.println("Sum of Odd Number: " +odd);
 			scanner.close();
 			
 
