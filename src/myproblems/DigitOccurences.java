6d78ef369f465b7cbdbf7146007a125d18269dbc
keerthi vutukuri <keerthivutukuri@gmail.com>
2023-04-09 22:37:52 +0100

    Digits Occurences count

---
+++ src/myproblems/DigitOccurences.java
@@ -0,0 +1,33 @@
+package myproblems;
+
+import java.util.Scanner;
+
+public class DigitOccurences {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a Number:");
+		Scanner scanner = new Scanner(System.in);
+		int  num = scanner.nextInt();
+		System.out.println("Enter your digit");
+		int digit  = scanner.nextInt(); 
+		int remainder;
+		int count = 0;
+		
+		if(num == 0 && digit ==0) 
+			count = 1;
+		while(num!=0) {
+			//count++;
+			
+			remainder = num % 10;
+			
+			if(digit == remainder) {
+				count++;
+			}
+			num = num/10;
+		}
+		
+		System.out.println("Total number of digits in your number " + count);
+	}
+
+}
