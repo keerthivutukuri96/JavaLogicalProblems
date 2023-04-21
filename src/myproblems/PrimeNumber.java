2b9de85cef9dcd1f0b08c8f92196a26297981eb5
keerthi vutukuri <keerthivutukuri@gmail.com>
2023-04-21 09:13:34 +0100

    Updated prime logic

--- src/myproblems/PrimeNumber.java
+++ src/myproblems/PrimeNumber.java
@@ -10,26 +10,30 @@ public class PrimeNumber {
 		System.out.println("enter n1 value");
 		System.out.println("enter n2 value");
 		Scanner sc = new Scanner(System.in);
-		//Reading numbers from user
+		// Reading numbers from user
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
-		//checking prime or not between the range n1 and n2 
-		//prime check function invokes everytime
+		// checking prime or not between the range n1 and n2
+		// prime check function invokes everytime
 		for (int i = n1; i <= n2; i++) {
 			if (primeCheck(i)) {
 				System.out.print(i + " ");
 			}
 		}
 	}
-	
-	//checking each number is whether prime or not and returns true or false
+
+	// checking each number is whether prime or not and returns true or false
 	public static boolean primeCheck(int n1) {
 		boolean isPrime = true;
-		for (int i = 2; i < n1; i++) {
+		if (n1 % 2 == 0) {
+			isPrime = false;
+		} else {
+			for (int i = 3; i <= n1 / 2; i = i + 2) {
 
-			if (n1 % i == 0)
-				isPrime = false;
+				if (n1 % i == 0)
+					isPrime = false;
 
+			}
 		}
 		return isPrime;
 	}
