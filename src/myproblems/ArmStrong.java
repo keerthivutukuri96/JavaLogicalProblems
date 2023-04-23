c13862ce1426d30df3a30ff0843d1dae2eb2c444
keerthi vutukuri <keerthivutukuri@gmail.com>
2023-04-23 10:23:29 +0100

    wrote armstrong number using method!

--- src/myproblems/ArmStrong.java
+++ src/myproblems/ArmStrong.java
@@ -8,17 +8,26 @@ public class ArmStrong {
 		System.out.println("Enter your number");
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
-		int num = number;
-		int sum = 0,digit;
-		while(number!=0) {
-			digit = number%10;
-			sum += (digit * digit * digit);
-			number = number/10;
-		}
-		if(num == sum)
-			System.out.println("It is a ArmStrong number");
+		if (armStrong(number))
+			System.out.println("It is an armstrong number");
 		else
-			System.out.println("It is not a ArmStrong number");
+			System.out.println("It is not an armstrong number");
+
+		sc.close();
+	}
+
+	public static boolean armStrong(int number) {
+		int num = number;
+		int sum = 0, digit;
+		while (number != 0) {
+			digit = number % 10;
+			sum += (digit * digit * digit);
+			number = number / 10;
+
+		}
+
+		return num == sum;
+
 	}
 
 }
